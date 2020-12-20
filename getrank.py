3f547a031ca905fcd981e117dbbbc51c36abad50
XDeviation <xth@LAPTOP-R1MR3AQT.localdomain>
2020-12-21 03:17:56 +0800

    debug 0xCCCCCCCC

--- getrank.py
+++ getrank.py
@@ -7,8 +7,8 @@ headers = {
 }
 
 
-def nowcoder(url) -> None:
-    for page in range(12):
+def nowcoder(url, pages=1) -> None:
+    for page in range(pages):
         params = {
             'page': page + 1,
         }
@@ -27,5 +27,5 @@ def nowcoder(url) -> None:
 
 
 if __name__ == "__main__":
-    url = 'https://ac.nowcoder.com/acm-heavy/acm/contest/real-time-rank-data?token=&id=10272&limit=0&_=1608463451483'  #在每场比赛榜单的 Network XHR 下面
-    nowcoder(url)
+    url = ''  #在每场比赛榜单的 Network XHR 下面
+    nowcoder(url, 1)  # pages 指榜单页数
--- work.sh
+++ work.sh
@@ -1,11 +1,11 @@
-# echo XiaomiBefore1
-# python3 rating.py > change/ICPC2020XiaomiBefore1 < contest/ICPC2020XiaomiBefore1
-# echo XiaomiBefore2
-# python3 rating.py > change/ICPC2020XiaomiBefore2 < contest/ICPC2020XiaomiBefore2
-# echo XiaomiFinal
-# python3 rating.py > change/ICPC2020XiaomiFinal < contest/ICPC2020XiaomiFinal
-# echo Shanghai
-# python3 rating.py > change/ICPC2020Shanghai < contest/ICPC2020Shanghai
+echo XiaomiBefore1
+python3 rating.py > change/ICPC2020XiaomiBefore1 < contest/ICPC2020XiaomiBefore1
+echo XiaomiBefore2
+python3 rating.py > change/ICPC2020XiaomiBefore2 < contest/ICPC2020XiaomiBefore2
+echo XiaomiFinal
+python3 rating.py > change/ICPC2020XiaomiFinal < contest/ICPC2020XiaomiFinal
+echo Shanghai
+python3 rating.py > change/ICPC2020Shanghai < contest/ICPC2020Shanghai
 echo Nanjing
 python3 rating.py > change/ICPC2020Nanjing < contest/ICPC2020Nanjing
 echo Final
