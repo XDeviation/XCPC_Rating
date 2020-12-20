5f02702265a60e590c84e6827415e9627a8601a3
XDeviation <xth@LAPTOP-R1MR3AQT.localdomain>
2020-12-21 02:11:47 +0800

    debug

--- getrank.py
+++ getrank.py
@@ -8,7 +8,7 @@ headers = {
 
 
 def nowcoder(url) -> None:
-    for page in range(19):
+    for page in range(12):
         params = {
             'page': page + 1,
         }
@@ -27,5 +27,5 @@ def nowcoder(url) -> None:
 
 
 if __name__ == "__main__":
-    url = ''  #在每场比赛榜单的 Network XHR 下面
+    url = 'https://ac.nowcoder.com/acm-heavy/acm/contest/real-time-rank-data?token=&id=10272&limit=0&_=1608463451483'  #在每场比赛榜单的 Network XHR 下面
     nowcoder(url)
