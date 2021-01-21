98da8bf12606b36436f4f7ad2e9dfb7fecb5b082
”XDeviation” <191602098@qq.com>
2021-01-21 18:29:42 +0800

    complete final rating

--- src/showrating.py
+++ src/showrating.py
@@ -8,20 +8,24 @@ def takerank(x):
     return (int)(x[2])
 
 
-db = TinyDB('../data/list.json', indent=4)
+db = TinyDB('data/data.json', indent=4)
 teamdata = Query()
 now = db.all()
 team = []
 for i in now:
     team.append((
-        i['member'],
+        i['members'],
         i['chschool'],
         i['rating'],
+        i['history'][len(i['history']) - 1]['team_name'],
     ))
-print("| Rank | School | Name | Rating |")
-print("| ---- | ---- | ---- | ---- |")
+print("| Rank | School | Name | Members | Rating |")
+print("| ---- | ---- | ---- | ---- | ---- |")
 team.sort(key=takerank, reverse=True)
 rank = 1
 for i in team:
-    print(f"| {rank} | {i[1]} | {i[0]} | {i[2]} |")
+    s = ''
+    for name in i[0]:
+        s = ' ' + name + s
+    print(f"| {rank} | {i[1]} | {i[3]} |{s} | {i[2]} |")
     rank += 1
--- work.sh
+++ work.sh
@@ -1,20 +1,18 @@
-echo CCPC2020Girl
-python3 src/rating.py > change/CCPC2020Girl < menu/CCPC2020Girl
-echo CCPC2020Qinghuangdao
-python3 src/rating.py > change/CCPC2020Qinghuangdao < menu/CCPC2020Qinghuangdao
-echo CCPC2020Weihai
-python3 src/rating.py > change/CCPC2020Weihai < menu/CCPC2020Weihai
-echo CCPC2020Mianyang
-python3 src/rating.py > change/CCPC2020Mianyang < menu/CCPC2020Mianyang
-echo CCPC2020Changchun
-python3 src/rating.py > change/CCPC2020Changchun < menu/CCPC2020Changchun
-echo ICPC2020Xiaomi
-python3 src/rating.py > change/ICPC2020Xiaomi < menu/ICPC2020Xiaomi
-echo ICPC2020Shanghai
-python3 src/rating.py > change/ICPC2020Shanghai < menu/ICPC2020Shanghai
-echo ICPC2020Nanjing
-python3 src/rating.py > change/ICPC2020Nanjing < menu/ICPC2020Nanjing
-echo ICPC2020Jinan
-python3 src/rating.py > change/ICPC2020Jinan < menu/ICPC2020Jinan
+# echo CCPC2020Girl
+# python3 src/rating.py > change/CCPC2020Girl < menu/CCPC2020Girl
+# echo CCPC2020Qinghuangdao
+# python3 src/rating.py > change/CCPC2020Qinghuangdao < menu/CCPC2020Qinghuangdao
+# echo CCPC2020Weihai
+# python3 src/rating.py > change/CCPC2020Weihai < menu/CCPC2020Weihai
+# echo CCPC2020Mianyang
+# python3 src/rating.py > change/CCPC2020Mianyang < menu/CCPC2020Mianyang
+# echo CCPC2020Changchun
+# python3 src/rating.py > change/CCPC2020Changchun < menu/CCPC2020Changchun
+# echo ICPC2020Shanghai
+# python3 src/rating.py > change/ICPC2020Shanghai < menu/ICPC2020Shanghai
+# echo ICPC2020Nanjing
+# python3 src/rating.py > change/ICPC2020Nanjing < menu/ICPC2020Nanjing
+# echo ICPC2020Jinan
+# python3 src/rating.py > change/ICPC2020Jinan < menu/ICPC2020Jinan
 echo Final
-python3 showrating.py > finalrating/README.md
+python3 src/showrating.py > finalrating/README.md
